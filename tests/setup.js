4bd542528d8939c06c1a9bd08a9f81fa06ff52ed
Dariia <drobotcko@gmail.com>
2021-03-28 16:26:42 +0300

    - fix useMongoClient

--- tests/setup.js
+++ tests/setup.js
@@ -1,7 +1,7 @@
-require('../models/User');
+require("../models/User");
 
-const mongoose = require('mongoose');
-const keys = require('../config/keys');
+const mongoose = require("mongoose");
+const keys = require("../config/keys");
 
 mongoose.Promise = global.Promise;
-mongoose.connect(keys.mongoURI, { userMongoClient: true });
+mongoose.connect(keys.mongoURI, { useMongoClient: true });
